1c33e99765a1c8559c8fb7c6c88cc23de6a9f57b
kaganalgul <kagan.algul06@gmail.com>
2021-12-22 12:53:43 +0300

    Home View Model

---
+++ src/Web/Services/HomeViewModelService.cs
@@ -0,0 +1,16 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+
+namespace Web.Services
+{
+    public class HomeViewModelService
+    {
+        private readonly IRepository<Product> _productRepository;
+
+        public HomeViewModelService(IRepository<Product> productRepository )
+        {
+            _productRepository=productRepository;
+        }
+
+    }
+}
